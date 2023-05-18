ebee7fc9f66bc1ab6b6435859a2111ee2ec68e93
Jovan Vujicic <jovan95vujicic@gmail.com>
2023-05-18 16:55:27 +0200

    Fix Faq.

--- src/components/Pages/Landing/Landing.tsx
+++ src/components/Pages/Landing/Landing.tsx
@@ -25,408 +25,408 @@ const Landing: FC = () => {
         <Participants />
         <Faq />
         {/* <Footer /> */}
-        <p>
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-          fsdfdsf
-          <br />
-        </p>
+        {/* <p> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/*  fsdfdsf */}
+        {/*  <br /> */}
+        {/* </p> */}
       </div>
     </div>
   );
