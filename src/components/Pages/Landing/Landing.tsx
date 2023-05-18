f477442efb8c39663993d9d30742aabc2244cc9b
Niko Beretic <nikoberetic@gmail.com>
2023-05-18 10:21:49 +0200

    Init

--- src/components/Pages/Landing/Landing.tsx
+++ src/components/Pages/Landing/Landing.tsx
@@ -18,6 +18,24 @@ const schedule = [
     description:
       'Lorem ipsum dolor sit amet, consectetur adipiscing pulvinar dapibus leo. Integer posuere erat a ante venenatis dapibus posuere velit aliquet.',
   },
+  {
+    type: 'Break',
+    time: '09:00 - 9:40',
+    label: 'Welcome Talk ',
+    author: 'John Evens',
+    title: 'Opening Ceremony and Introduce the Event.',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing pulvinar dapibus leo. Integer posuere erat a ante venenatis dapibus posuere velit aliquet.',
+  },
+  {
+    type: 'Workshop',
+    time: '09:00 - 9:40',
+    label: 'Welcome Talk ',
+    author: 'John Evens',
+    title: 'Opening Ceremony and Introduce the Event.',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing pulvinar dapibus leo. Integer posuere erat a ante venenatis dapibus posuere velit aliquet.',
+  },
 ];
 
 const Landing: FC = () => {
