78ae6ebbdf7151b3bc0b0b0973feb649ab83c01e
Niko Beretic <nikoberetic@gmail.com>
2023-05-22 15:30:53 +0200

    Update

--- src/components/Pages/Landing/DescriptionSection/DescriptionSection.tsx
+++ src/components/Pages/Landing/DescriptionSection/DescriptionSection.tsx
@@ -7,13 +7,13 @@ const DescriptionSection: FC = () => (
   <div className={styles['description-section-wrapper']}>
     <p>
       Compute Camp is a yearly gathering of compute science enthusiasts innovating in the
-      different computing paradigms, from web3, databases, cloud, virtual machines or
-      pushing the frontiers of what&apos;s possible with computing.
+      different computing paradigms, including web3, databases, cloud, virtual machines,
+      or pushing the frontiers of what&apos;s possible with computing.
       <br />
       <br />
-      The focus of the first iteration of the event will have a particular emphasis on
-      web3 as Compute Camp started from the Filecoin & IPFS community, with the objective
-      to spread the knowledge.
+      Springing from the momentum of the Filecoin and IPFS Communities, the focus of this
+      first iteration of Compute Camp will have a strong emphasis on web3 with the main
+      objective of spreading knowledge and growing the web3 community.
     </p>
     <Illustration2 />
   </div>
