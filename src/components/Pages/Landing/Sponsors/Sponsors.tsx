2f67e14e4b87f7dc228d7bbf21ece32b0f7e88da
Niko Beretic <nikoberetic@gmail.com>
2023-05-22 15:57:41 +0200

    Update

--- src/components/Pages/Landing/Sponsors/Sponsors.tsx
+++ src/components/Pages/Landing/Sponsors/Sponsors.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image';
 import SectionTitle from 'components/Common/SectionTitle/SectionTitle';
 import LogoWeb3mine from 'components/Common/Icons/LogoWeb3mine';
 import LogoPL from 'components/Common/Icons/LogoPL';
+import LogoFluence from 'components/Common/Icons/LogoFluence';
 import templateLogo from './logos/sponsors-template-logo.png';
 
 import styles from './Sponsors.module.scss';
@@ -23,7 +24,7 @@ const Sponsors: FC = () => {
           <div className={styles.logos}>
             <LogoWeb3mine />
             <LogoPL />
-            <Image src={templateLogo} alt="templateLogo" />
+            <LogoFluence />
           </div>
 
           <div className={styles.logos}>
