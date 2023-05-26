28e12c526facb51f0a31e4ee307befa1da4a4e40
Niko Beretic <nikoberetic@gmail.com>
2023-05-26 15:35:38 +0200

    Update

--- next.config.js
+++ next.config.js
@@ -1,20 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  async headers() {
-    return [
-      {
-        source: '/:all*(svg|jpg|png)',
-        locale: false,
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 'public, max-age=9999999999, must-revalidate',
-          }
-        ],
-      },
-    ]
-  },
   eslint: {
     dirs: [
       'src/assets',
